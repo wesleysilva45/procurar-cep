8a10202f596ebd38572a16a7c42a57304fe03ea4
unknown <wesley silva>
2024-04-05 15:38:05 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,50 @@
+async function consultarCEP() {
+    try {
+        const cepInput = document.getElementById('meuInput');
+        let cep = cepInput.value;
+  
+        // Formata o CEP
+        cep = cep.replace(/\D/g, "");
+        cep = cep.replace(/^(\d{5})(\d)/, "$1-$2");
+  
+        const url = `https://viacep.com.br/ws/${cep}/json/`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+  
+        // Exiba as partes do CEP no HTML
+        const resultadoDiv = document.getElementById('result');
+        resultadoDiv.innerHTML = `
+            <p><strong>Cidade:</strong> ${data.localidade}</p>
+            <p><strong>Logradouro:</strong> ${data.logradouro}</p>
+            <p><strong>Bairro:</strong> ${data.bairro}</p>
+            <p><strong>CEP:</strong> ${cep}</p>
+        `;
+    } catch (error) {
+        console.error('Erro ao consultar o CEP:', error);
+    }
+  }
+function limparTela(){
+    const resultadoDiv = document.getElementById('result');
+    resultadoDiv.innerHTML = '';
+
+};
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('Nice, you triggered this alert message!', 'success')
+  })
+}
